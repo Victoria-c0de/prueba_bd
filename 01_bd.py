2c3c30b4646c938abae9d39d99956e0beb61735b
Victoria Molina C <victoria.developer023@gmail.com>
2023-11-29 14:37:05 -0400

    probando

---
+++ 01_bd.py
@@ -0,0 +1,15 @@
+import time
+ 
+ 
+ 
+inicio = time.time()
+print(inicio)
+
+#El programa debe calcular el monto a cobrar por el tiempo de permanencia de un vehículo en el estacionamiento.
+#El usuario debe ingresar la hora de entrada y la hora de salida.
+
+#El programa debe imprimir el monto a cobrar.
+#el programa debe mostrar la patente 
+
+
+
---
+++ probando.py
@@ -0,0 +1,7 @@
+#El programa debe calcular el monto a cobrar por el tiempo de permanencia de un vehículo
+#en el estacionamiento
+
+#el egreso es pagado siempre.
+#El monto a cobrar se calcula de la siguiente manera:
+#Por permanencias inferiores a 4 minutos se cobran $100. 
+
